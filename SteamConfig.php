bf4a3f9a10589ba98a5c29f78b52f64ff3832abc
Ethan <32881589+XaFlaForo@users.noreply.github.com>
2019-03-30 22:26:25 +0000

    Add files via upload

---
+++ SteamConfig.php
@@ -0,0 +1,7 @@
+<?php
+//Version 4.0
+$steamauth['apikey'] = "D26C50C183FE1151ADBBE0191F2F37C4"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "http://localhost/AltisLife-StatsPanel/displayStats.php"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+?>
